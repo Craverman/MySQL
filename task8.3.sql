7784056de4ccff57f66e0b77005c4a75c4307530
Zakharenkov Vladislav <85833732+Craverman@users.noreply.github.com>
2021-10-15 01:56:35 +0300

    Revert "Lesson 8"
    This reverts commit 1474d3a0c15bcba23104482e2694754f0f15f750.

--- task8.3.sql
+++
@@ -1,9 +0,0 @@
-select (
-select count(*) from likes left join profiles
-	on likes.user_id = profiles.user_id 
-	where profiles.gender = 'f') as female, (
-select count(*) from likes left join profiles
-	on likes.user_id = profiles.user_id 
-	where profiles.gender = 'm') as male;
-
-
